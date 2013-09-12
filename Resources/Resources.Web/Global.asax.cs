77efdc0b9ac823431b1d2eaebf3de57e1e5e91f0
unknown <herrad.koe@gmail.com>
2013-09-12 20:34:24 +0100

    setting up structure map ObjectFactory in Resources.Web

--- Resources/Resources.Web/Global.asax.cs
+++ Resources/Resources.Web/Global.asax.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using Resources.Web.StructureMap;
 
 namespace Resources.Web
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            new ResourcesStructureMapConfiguration().Configure();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
